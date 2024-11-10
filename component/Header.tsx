341b234cd784109f710321dff6aef55a3778d6a8
Shagufta Zakir <159715160+Umar-30@users.noreply.github.com>
2024-11-10 23:07:26 +0500

    This is figma desinge

---
+++ component/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="w-[1920px] h-[134px] bg-[#A29875]">
+      <div className='w-[439px] h-[94px] mt-[20px] ml-[36px] leading-[93.75px] text-[75px] text-white'>MANZZARI</div>
+
+    </div>
+  )
+}
+
+export default Header
